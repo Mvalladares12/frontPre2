71105a258a4c715435d5d0957d2b32b039995871
Mario <mariovalladares178@gmail.com>
2026-05-01 22:35:07 -0600

    commit2

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true, // En tu local debe decir false
   apiUrl: 'http://localhost:8084',
-  keycloakUrl: 'https://auth-keycloak-pre.uk', // Asumiendo que tu Keycloak local está en el 8180
+  keycloakUrl: 'http://localhost:8180', // Asumiendo que tu Keycloak local está en el 8180
   keycloakRealm: 'seguridad-app',
   keycloakClient: 'front-app2'
 };
